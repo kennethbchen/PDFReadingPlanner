269bf34268fa05b6b974a258f573d842b31b72ca
kennethbchen <kennethbchen@gmail.com>
2020-09-19 18:32:21 -0400

    Streamlined Methods

--- Main.py
+++ Main.py
@@ -1,24 +1,6 @@
 import fitz
 from math import ceil
 
-filename = 'ignore/soci.pdf'
-doc = fitz.open(filename)
-ol = doc.outline
-beginning_offset = 41
-
-
-def get_internal_page(book_page):
-    """
-    Gets the internal page number based on the beginning_offset.
-    This is needed because PDFs may number their pages starting later in the book.
-    For example, the PDF may have a preface section which is not numbered in the same
-    way as the table of contents. This means the table of contents won't match up with
-    the actual page number in the PDF
-    :param book_page: Page based on the table of contents to convert
-    :return: converted page number
-    """
-    return book_page + beginning_offset
-
 
 def get_outline_structure(outline, level=0):
     """ Gets the full tree structure of an outline.
@@ -39,27 +21,27 @@ def get_outline_structure(outline, level=0):
     return output
 
 
-def get_parent_tree(outline_structure, start):
+def get_parent_tree(outline_structure, internal_start_page):
     """
     Gets the outline structure of one branch starting at a page number
     start is the page number based on the table of contents of the PDF
     :param outline_structure: List of the outline contents
-    :param start: the first page of the tree you want to select
+    :param internal_start_page: the first page of the tree you want to select
 
     :rtype [level <int>, page <int>, title <str>]
     :return: List that represents structure of the outline range
     """
-    start = get_internal_page(start)
+
     output = []
     max_level = -1
 
     for item in outline_structure:
-        if item[1] == start - 1:
+        if item[1] == internal_start_page - 1:
             output.append(item)
             max_level = item[0]
             continue
 
-        if (item[1] >= start) and (item[0] > max_level):
+        if (item[1] >= internal_start_page) and (item[0] > max_level):
             output.append(item)
         elif (len(output) > 0) and (item[0] == max_level):
             output.append(item)
@@ -71,7 +53,7 @@ def get_parent_tree(outline_structure, start):
 def get_page_counts(outline_structure):
     for val in range(0, len(outline_structure)):
         if val > 0:
-            outline_structure[val - 1].append(outline_structure[val][1] - outline_structure [val - 1][1])
+            outline_structure[val - 1].append(outline_structure[val][1] - outline_structure[val - 1][1])
 
     del outline_structure[-1]  # We got one extra heading in order to do page count so remove it
     return outline_structure
@@ -84,10 +66,8 @@ def partition_outline(outline_structure, days):
     for item in outline_structure:
         total_pages += item[3]
     pages_per_day = ceil(total_pages / days)
-    print(pages_per_day)
     count = 0
     for item in outline_structure:
-        print(item)
         if (count + item[3]) <= pages_per_day:
             count += item[3]
             buffer.append(item)
@@ -99,15 +79,24 @@ def partition_outline(outline_structure, days):
     if len(buffer) != 0:
         output.append(buffer)
 
-    return output
+    return pages_per_day, output
 
 
-structure = get_page_counts(get_parent_tree(get_outline_structure(ol), 198))
-structure.pop(-1)
-structure.pop(-1)
+def generate_plan(document, toc_start_page, days, toc_offset=0, trim_by=0):
+    ol = document.outline
+    headings = get_page_counts(get_parent_tree(get_outline_structure(ol), toc_start_page + toc_offset))
+    for i in range(0,trim_by):
+        headings.pop(-1)
 
-for val in partition_outline(structure, 5):
-     print(val)
+    pages_per_day, partition = partition_outline(headings, days)
+    return pages_per_day, partition
 
 
+filename = 'ignore/soci.pdf'
+doc = fitz.open(filename)
 
+rate, plan = generate_plan(doc, 250, 5, 41, 2)
+
+print(rate)
+for val in plan:
+    print(val)
